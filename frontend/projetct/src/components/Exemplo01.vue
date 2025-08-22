be98dad88b2d0bf94bc6b90c71278be0e3533b0e
superkarlos <jcarlos.alegria2015@gmail.com>
2025-08-22 11:52:48 -0300

    feat : introdução

---
+++ frontend/projetct/src/components/Exemplo01.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  nome: {
+    type: String,
+    required: true,
+    default: 'Alice'
+  }
+})
+</script>
+
+<template>
+  <section class="card">
+    <h1 class="title">Nome: {{ props.nome }}</h1>
+
+  </section>
+</template>
+
+<style scoped>
+.card {
+  padding: 1.5rem 2rem;
+  background: #ffffff;
+  border-radius: 16px;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+  max-width: 400px;
+  margin: 1rem auto;
+  text-align: center;
+  transition: transform 0.2s;
+}
+
+.card:hover {
+  transform: translateY(-5px);
+}
+
+.title {
+  color: #333333;
+  font-family: 'Segoe UI', Tahoma, sans-serif;
+  font-size: 1.5rem;
+  margin-bottom: 1rem;
+}
+</style>
---
+++ frontend/projetct/src/main.js
@@ -0,0 +1,6 @@
+//modulo de criar
+import { createApp } from 'vue'
+import App from './App.vue'
+
+//exibe tudo
+createApp(App).mount('#app')
